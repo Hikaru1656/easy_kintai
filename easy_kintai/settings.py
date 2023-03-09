19b8fbb2cbe9b495fc27527f22509f9a2ff8485f
Hikaru1656 <shining-olaf@outlook.jp>
2023-03-09 23:34:15 +0900

    heroku エラー修正

--- easy_kintai/settings.py
+++ easy_kintai/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "accounts",
     "attendance",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -60,7 +62,10 @@ ROOT_URLCONF = "easy_kintai.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+             os.path.join(BASE_DIR,'templates','allauth'),
+            os.path.join(BASE_DIR,'templates'),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -174,3 +179,5 @@ if not DEBUG:
 
 import django_heroku
 django_heroku.settings(locals())
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
