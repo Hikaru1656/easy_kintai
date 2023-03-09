670ca06f30ab578f2fe3813f9e33efbc0824363b
Hikaru1656 <shining-olaf@outlook.jp>
2023-03-10 02:28:34 +0900

    サーバーエラー表示

--- attendance/urls.py
+++ attendance/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+handler500 = views.my_customized_server_error
+
 
 urlpatterns = [
     path('owner/attends/', views.attends, name='attends'),
