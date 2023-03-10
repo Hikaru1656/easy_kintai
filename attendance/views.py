2c507330f4341d7a2df992aad36539111a3c3845
Hikaru1656 <shining-olaf@outlook.jp>
2023-03-10 12:22:52 +0900

    views.payments修正

--- attendance/views.py
+++ attendance/views.py
@@ -101,7 +101,7 @@ def payments(request):
             attends = Attend.objects.filter(user=user_data.id)
             payments_logs[user_data.username] = attends
             total_salary = 0
-            for attend in attends:
+            for key, attend in attends.items():
                 total_salary += attend.salary
             salary_logs[user_data.username] = total_salary
         params = {
