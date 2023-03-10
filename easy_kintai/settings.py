c47f24ff4275af71f00677721153fbaf981a36c3
Hikaru1656 <shining-olaf@outlook.jp>
2023-03-10 15:53:08 +0900

    settings修正

--- easy_kintai/settings.py
+++ easy_kintai/settings.py
@@ -87,7 +87,8 @@ WSGI_APPLICATION = "easy_kintai.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
